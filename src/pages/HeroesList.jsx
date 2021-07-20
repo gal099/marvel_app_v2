099f83a1f77dc55b0195e6fefdc0f6f52bc4919e
Juan <jb@hotmail.com>
2021-07-20 16:30:24 -0300

    modifico componente

--- src/pages/HeroesList.jsx
+++ src/pages/HeroesList.jsx
@@ -1,5 +1,11 @@
 import { HeroesGrid } from '../components/HeroesGrid';
+import { TitlePage } from '../components/TitlePage';
 
 export function HeroesList() {
-  return <HeroesGrid />;
+  return (
+    <>
+      <TitlePage text={'Lista de Heroes'} />
+      <HeroesGrid />
+    </>
+  );
 }
