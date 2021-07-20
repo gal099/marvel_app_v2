01174aff11191d7f6e7c19d4f74a0fb28603f58e
Juan <jb@hotmail.com>
2021-07-20 12:39:12 -0300

    creo módulo - aplico estilos y propiedades

--- src/HeroCard.jsx
+++ src/HeroCard.jsx
@@ -5,7 +5,13 @@ export function HeroCard(props) {
 
   return (
     <li className={styles.heroCard}>
-      <img className={styles.heroImage} src={imageUrl} alt={props.hero.name} />
+      <img
+        width={230}
+        height={230}
+        className={styles.heroImage}
+        src={imageUrl}
+        alt={props.hero.name}
+      />
       <div>{props.hero.name}</div>
     </li>
   );
--- src/HeroesGrid.jsx
+++ src/HeroesGrid.jsx
@@ -1,11 +1,12 @@
 import { HeroCard } from './HeroCard';
 import heroes from './heroes.json';
+import styles from './HeroesGrid.module.css';
 
 const hero = heroes.data.results;
 
 export function HeroesGrid() {
   return (
-    <ul>
+    <ul className={styles.heroesGrid}>
       {hero.map((elem) => (
         <HeroCard key={elem.id} hero={elem} />
       ))}
