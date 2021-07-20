dea5d520458d4c55e6b2fed0c0f32db04c1eed19
Juan <jb@hotmail.com>
2021-07-20 16:31:03 -0300

    modifico componente - agrego estilos

--- src/pages/HeroDetails.jsx
+++ src/pages/HeroDetails.jsx
@@ -1,16 +1,28 @@
 import hero from '../components/heroDetails.json';
 import styles from './HeroDetails.module.css';
+import { TitlePage } from '../components/TitlePage';
 
 export function HeroDetails() {
   const imageUrl = `${hero.thumbnail.path}.${hero.thumbnail.extension}`;
   console.log(hero);
   return (
-    <div className={styles.detailsContainer}>
-      <img src={imageUrl} alt={hero.name} />
-      <div>
-        <p>{hero.name} </p>
-        <p>{hero.description}</p>
+    <>
+      <TitlePage text={'Detalle del Heroe'} />
+      <div className={styles.detailsContainer}>
+        <img
+          className={`${styles.col} ${styles.heroImage}`}
+          src={imageUrl}
+          alt={hero.name}
+        />
+        <div className={`${styles.col} ${styles.heroDetails}`}>
+          <p className={styles.firstLine}>
+            <strong>Nombre:</strong> {hero.name}{' '}
+          </p>
+          <p>
+            <strong>Descrpción:</strong> {hero.description}
+          </p>
+        </div>
       </div>
-    </div>
+    </>
   );
 }
