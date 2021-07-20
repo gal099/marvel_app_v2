f92f39eca7406eefa3493d3e037701b8ddfe7898
Juan <jb@hotmail.com>
2021-07-20 15:53:33 -0300

    agrego pagina y componente de estilo

--- src/pages/HeroDetails.jsx
+++ src/pages/HeroDetails.jsx
@@ -1,3 +1,16 @@
+import hero from '../components/heroDetails.json';
+import styles from './HeroDetails.module.css';
+
 export function HeroDetails() {
-  return <div>detallesssss</div>;
+  const imageUrl = `${hero.thumbnail.path}.${hero.thumbnail.extension}`;
+  console.log(hero);
+  return (
+    <div className={styles.detailsContainer}>
+      <img src={imageUrl} alt={hero.name} />
+      <div>
+        <p>{hero.name} </p>
+        <p>{hero.description}</p>
+      </div>
+    </div>
+  );
 }
