5893427aafeb2bfbc9cd9fecaf1f2e333185a534
Juan <jb@hotmail.com>
2021-07-20 11:56:25 -0300

    creo const para el array

--- src/HeroesGrid.jsx
+++ src/HeroesGrid.jsx
@@ -1,10 +1,12 @@
 import { HeroCard } from './HeroCard';
 import heroes from './heroes.json';
 
+const hero = heroes.data.results;
+
 export function HeroesGrid() {
   return (
     <ul>
-      {heroes.data.results.map((elem) => (
+      {hero.map((elem) => (
         <HeroCard key={elem.id} hero={elem} />
       ))}
     </ul>
