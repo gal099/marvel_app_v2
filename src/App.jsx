6d73c2bb357079e4fafef76d84ff5cb79fd1479c
Juan <jb@hotmail.com>
2021-07-20 10:45:35 -0300

    cambio extension y modifico contenido

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import { HeroesGrid } from './HeroesGrid';
+
+function App() {
+  return (
+    <div>
+      <header>
+        <h1>Heroes</h1>
+      </header>
+      <main>
+        <HeroesGrid />
+      </main>
+    </div>
+  );
+}
+
+export default App;
