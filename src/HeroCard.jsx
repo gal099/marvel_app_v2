cac8047397e7ed5ac04645402fb3fdc59ab241a0
Juan <jb@hotmail.com>
2021-07-20 12:22:04 -0300

    importo módulo css  - aplico estilos

--- src/HeroCard.jsx
+++ src/HeroCard.jsx
@@ -1,4 +1,12 @@
+import styles from './HeroCard.module.css';
+
 export function HeroCard(props) {
-  console.log(props);
-  return <li> {props.hero.name} </li>;
+  const imageUrl = `${props.hero.thumbnail.path}.${props.hero.thumbnail.extension}`;
+
+  return (
+    <li className={styles.heroCard}>
+      <img className={styles.heroImage} src={imageUrl} alt={props.hero.name} />
+      <div>{props.hero.name}</div>
+    </li>
+  );
 }
