a5ef149623d88b6ce54b6bf7edb425ba1c6f3ad5
Juan <jb@hotmail.com>
2021-07-28 16:26:01 -0300

    uso fetch para conectar con la api

--- src/components/HeroesGrid.jsx
+++ src/components/HeroesGrid.jsx
@@ -1,14 +1,22 @@
+import { useEffect, useState } from 'react';
 import { HeroCard } from './HeroCard';
-import heroes from './heroes.json';
 import styles from './HeroesGrid.module.css';
 
-const hero = heroes.data.results;
-
 export function HeroesGrid() {
+  const [heroes, setHeroes] = useState([]);
+
+  useEffect(() => {
+    fetch(
+      `https://gateway.marvel.com:443/v1/public/characters?apikey=e7d57fb50cf5c7cd3da6c4bab80a741e&hash=9bae1d7bc80738cf67190f0d44a60163&ts=1626185133165`
+    )
+      .then((result) => result.json())
+      .then((data) => setHeroes(data.data.results));
+  }, []);
+
   return (
     <>
       <ul className={styles.heroesGrid}>
-        {hero.map((elem) => (
+        {heroes.map((elem) => (
           <HeroCard key={elem.id} hero={elem} />
         ))}
       </ul>
